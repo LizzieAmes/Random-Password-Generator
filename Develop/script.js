77baef81a413a556ed306b4d45195a8ddc8b3292
Lizzie <lizziejacoboo@gmail.com>
2023-11-06 09:50:19 -0700

    Finished javascript code.

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,52 @@
-// Assignment code here
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Password criteria variables
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
 var specialCharacter = "!#$%&'()*+,-./:;<=>?[]^_`{|}~";
 
+// Function to generate a password and return if invalid length
 function generatePassword() {
   var passwordLength = prompt("Enter desired password length (between 8 and 128 characters.):");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter valid password length.");
+    return "";
+  }
+
+  // Variables for password prompts
+  var useLowercase = confirm("Include lowercase characters?");
+  var useUppercase = confirm("Include uppercase characters?");
+  var useNumeric = confirm("Include numeric characters?");
+  var useSpecial = confirm("Include special characters?");
+
+  // Combines users selections to create a password
+  var validCharacters = "";
+    if (useLowercase) validCharacters += lowerCase;
+    if (useUppercase) validCharacters += upperCase;
+    if (useNumeric) validCharacters += numeric;
+    if (useSpecial) validCharacters += specialCharacter;
+
+  //Generate the password
+  var password = "";
+    for (var i = 0; i < passwordLength; i++){
+      var randomIndex = Math.floor(Math.random() * validCharacters.length); 
+      password += validCharacters[randomIndex];
+    }
+    return password;
 }
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
+  // Write password to the #password input
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password Generator Starter Code
+# Random Password Generator 
+This webpage generates a random password according to selections that are made by the user. This can help create complex and secure passwords for people to use.
+The webpage includes a button for the user to click to start the password generator. This will send promts to the user asking if they would like upper case, lower case, numbers or special characters included in their new password. After these prompts are answered, the page will then generate the new secure password. 
+
+
+![Random Password Generator](develop/assets/images/Random-password-generator.png)
+
+## Resources Used
+During this assignment, I watched SuperSimpleDev's youtube video: https://www.youtube.com/watch?v=SBmSRK3feww&t=16483s. This gave me a better understanding on how javascript works.
